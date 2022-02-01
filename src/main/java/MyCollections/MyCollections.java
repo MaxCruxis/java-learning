861e066e2887b3dab9a2386e4dfebb34f87bc4fe
MaxCruxis <mailforprog@yandex.ru>
2022-02-01 22:52:45 +0300

    tried more features of Map

--- src/main/java/MyCollections/MyCollections.java
+++ src/main/java/MyCollections/MyCollections.java
@@ -6,7 +6,7 @@ public class MyCollections {
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_RED = "\u001B[31m";
-    /* private void arrayList() {
+     private void arrayList() {
          int[] array;
          array = new int[10];
          array[1] = 2;
@@ -89,13 +89,13 @@ public class MyCollections {
              System.out.println(entry.getKey()+ " " + entry.getValue());
          }
  //        for (int i = 0; i <= 10; i++) System.out.println((char)(i+33) );
-     }*/
+     }
     private void mapInList() {
         List<String> keyList = new ArrayList<>();
         List<String> valueList = new ArrayList<>();
         List<Map<String, String>> entryList = new ArrayList<>();
         for (int i = 0; i <= 15; i++) keyList.add(Integer.toHexString(i));
-        for (int i = 0; i <= 111; i++) valueList.add(Character.toString((char) (i + 32)));
+        for (int i = 0; i <= 255; i++) valueList.add(Character.toString((char) (i + 32)));
         for (int i = 0; i <= valueList.size() - 1; i++) {
             Map<String, String> a = new HashMap<>();
             for (int j = 0; j <= keyList.size() - 1; j++) {
@@ -112,15 +112,45 @@ public class MyCollections {
 
         }
         System.out.println();
-        int a =0;
-        for (int i = 0; i <= ((valueList.size())/(keyList.size()) -1); i++) {//112
-            System.out.print(i + 1 + " ");
+        int a =0; // for unique value each 16 keys else there would repeat first 16
+        for (int i = 0; i <= ((valueList.size())/(keyList.size()) -1); i++) {//  112/16 coz only 7 rows
+            System.out.print(Integer.toHexString(i) + " "); //for column numbers
             for (int j = 0; j <= keyList.size() - 1; j++) { //16
                 System.out.print(entryList.get(j+a).get(keyList.get(i)) + " ");
             }
             a=a+keyList.size();
             System.out.println();
         }
+        int x = Integer.parseInt("9");
+        double c = Double.parseDouble("5");
+        int b = Integer.parseInt("444",16);
+
+        System.out.println(x);
+        System.out.println(c);
+        System.out.println(b);
+    }
+    private void mapsMerge(){
+        HashMap<Integer, String> passportsAndNames = new HashMap<>();
+        HashMap<Integer, String> passportsAndNames2 = new HashMap<>();
+
+        passportsAndNames.put(212133, "Walter White");
+        passportsAndNames.put(162348, "Donald Trump");
+        passportsAndNames.put(8082771, "Conan O'Brien");
+        passportsAndNames.put(925648, "Jimi Hendrix");
+
+        passportsAndNames2.put(917352, "Vladimir Putin");
+        passportsAndNames2.put(925648, "Pavel Durov");
+
+
+
+        passportsAndNames.putAll(passportsAndNames2);
+        for (Map.Entry entry : passportsAndNames.entrySet()){
+            System.out.println(entry);
+        }
+        Set<Integer> keys = passportsAndNames.keySet();
+
+        System.out.println(keys);
+
     }
 
     public static void main(String[] args) {
@@ -128,7 +158,8 @@ public class MyCollections {
 //        new MyCollections().findChar(1);
 //        new MyCollections().setExploring();
 //        new MyCollections().mapExploring();
-        new MyCollections().mapInList();
+        new MyCollections().mapsMerge();
+//        new MyCollections().mapInList();
     }
 
 
