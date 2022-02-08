694c19d04d4fcf0c01404d61987bca25d333f45a
MaxCruxis <mailforprog@yandex.ru>
2022-02-08 16:56:57 +0300

    tested interrupt

--- src/main/java/Concurrency/ThreadStudying.java
+++ src/main/java/Concurrency/ThreadStudying.java
@@ -1,17 +1,32 @@
 package Concurrency;
 
+import java.util.concurrent.TimeUnit;
+
 public class ThreadStudying {
     public static void main(String[] args) {
-        Runnable task = () -> {
+        /*Runnable task = () -> {
             try {
-                int secToWait = 1000 * 60;
-                Thread.currentThread().sleep(secToWait);
+                *//*int secToWait = 1000 * 3;
+                Thread.currentThread().sleep(secToWait);*//*
+                System.out.println("nap time");
+                TimeUnit.SECONDS.sleep(5);
                 System.out.println("Wake the fck up Samurai");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         };
         Thread thread = new Thread(task);
+        thread.start();*/
+
+        Runnable task = () -> {
+            try {
+                TimeUnit.SECONDS.sleep(60);
+            } catch (InterruptedException e) {
+                System.out.println("Interrupted");
+            }
+        };
+        Thread thread = new Thread(task);
         thread.start();
+        thread.interrupt();
     }
 }
